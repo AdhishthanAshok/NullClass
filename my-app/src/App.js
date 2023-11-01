d90f3cd48104e91dd5fe1f3a3674a9066e28d98f
Adhishthan <adhishthanashok@gmail.com>
2023-11-01 22:45:22 +0530

    Started StackOverflow Clone App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { HelloWorld, ByeWorld } from "./components/HelloWorld";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  console.log(counter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <p>Hello my name is Adhishthan</p>
+      <HelloWorld text="Hello How are you!" />
+      <ByeWorld />
+      <button onClick={increment}>Click Me</button>
+      <h1>{counter}</h1>
     </div>
   );
 }
---
+++ my-app/src/components/HelloWorld.jsx
@@ -0,0 +1,6 @@
+export function HelloWorld({ text }) {
+  return <h1>{text}</h1>;
+}
+export function ByeWorld() {
+  return <h4>Hii</h4>;
+}
